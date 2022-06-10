42fb4a1c46cfde5ed93e7b819934521bcb0c5f81
Tara <taraekin@hotmail.com>
2022-06-10 16:26:31 +0300

    Small changes

--- src/main/java/com/tara/ForeignExchangeApplication/web/controller/DemoController.java
+++ src/main/java/com/tara/ForeignExchangeApplication/web/controller/DemoController.java
@@ -12,14 +12,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.datetime.DateFormatter;
-import org.springframework.format.datetime.DateFormatterRegistrar;
-import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -35,26 +31,26 @@ public class DemoController {
     private final ConversionDao conversionDao;          // bunun yeri burası mı?
 
     @ApiOperation(value = "Create exchange demo")
-    @GetMapping("/getInfo")
+    @GetMapping("/info")
     public String getInfo() {
         return "uçusa hazır";
     }
 
     @ApiOperation(value = "Get exchange rate (version 1)")
-    @GetMapping("/getRate/{source},{target}")
+    @GetMapping("/rate/{source},{target}")
     public float getRate(@PathVariable("source") String source, @PathVariable("target") String target) throws Exception {
         return exchangeService.getExchangeRate(source, target);
     }
 
     @ApiOperation(value = "Get exchange rate (version 2)")
-    @PostMapping("/getRate/")
+    @PostMapping("/rate")
     // burada success kontrolü ile sadece float mu dönsek
     public ExchangeRate getRate2(@RequestBody ExchangeRateRequest request) throws Exception {
         return exchangeServiceV2.getExchangeRate(request);
     }
 
     @ApiOperation(value = "Make conversion")
-    @PostMapping("/convert/")
+    @PostMapping("/conversion")
     public Conversion convert(@RequestBody ConversionRequest request) throws Exception {
         Conversion conversion = conversionService.makeConversion(exchangeServiceV2.getExchangeRate(request.getExchangeRateRequest()), request);
         if (conversion.isSuccess()) {
@@ -64,23 +60,24 @@ public class DemoController {
     }
 
     @ApiOperation(value = "List all conversions made")
-    @GetMapping("/listAll/")
+    @GetMapping("/list")
     public List<Conversion> listAllConversions() {
         return conversionDao.selectAllTransactions();
     }
 
     @ApiOperation(value = "Filter conversions by ID")
-    @GetMapping("/getConversionByID/{id}")
+    @GetMapping("/conversion-by-id/{id}")
     public Optional<Conversion> getConversionById(@PathVariable("id") UUID id) {
         return conversionDao.selectTransactionById(id);
         // "No such transaction" diye bir mesaj ekleyebiliriz. şu an bu durumda null dönüyor.
     }
 
     @ApiOperation(value = "Filter conversions by date")
-    @GetMapping("/getConversionsByDate/{date}")
-    public Stream<Conversion> getConversionByDate(@PathVariable("date")
+    @GetMapping("/conversions-by-date/{date}")
+    public Stream<Conversion> getConversionByDate(/*@Validated*/ @PathVariable("date")
                                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                                                     LocalDate date) {
+        // geçersiz tarihler için hata mesajı dönmeliyiz
         return conversionDao.selectTransactionByDate(date);
     }
 }
